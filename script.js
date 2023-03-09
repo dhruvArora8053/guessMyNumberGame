41b1e3206347c3773e36f90de73e069d7b897422
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-09 18:38:43 +0530

    implemented the game logic

--- script.js
+++ script.js
@@ -10,13 +10,35 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   //   console.log(document.querySelector('.guess').value);
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+  //   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'too high';
+      document.querySelector('.score').textContent = score--;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'too low';
+      document.querySelector('.score').textContent = score--;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
   //when we get something from the input, it's always a string
 });
