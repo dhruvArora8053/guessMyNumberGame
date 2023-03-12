6d115c7cf1dadb5b67d8790449af7826f250c2d0
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-12 14:15:44 +0530

    guess my number game implemented

--- script.js
+++ script.js
@@ -89,3 +89,4 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
